5e03f0329e55dc665e3655478471cc6ab99557e6
marco <marco_silviu2000@yahoo.com>
2021-11-08 17:19:47 +0200

    Client login DialogBoxes

--- lib/api_connection/api_connection.dart
+++ lib/api_connection/api_connection.dart
@@ -1,48 +1,57 @@
-
-
 import 'dart:convert';
 
-import 'package:personal_dashboard_frontend/Data/user.dart';
 import 'package:http/http.dart' as http;
-// Future<List<User>>fetchUserList() async
-// {
-//   final response =
-//       await http.get(Uri.http("localhost8000", "api/user/"));
-//
-//   if (response.statusCode == 200) {
-//     var responseJson = json.decode(response.body);
-//     return (responseJson as List).map((p) => MenuItem.fromJson(p)).toList();
-//   }
-//   throw Exception('Failed to load client list');
-// }
 
 
-void sendAccForToken() async
-{
+Future<bool> checkIfUserExists(username, password, token) async {
+  Map<String, dynamic> map = {'username': username, 'password': password};
 
-  Map<String, dynamic> map =
+  final response = await http.put(
+    Uri.http("10.0.2.2:8000", "api/user/"),
+    body: jsonEncode(map),
+    headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+      'Authorization': 'TOKEN ' + await token,
+    },
+  );
+
+
+  switch(response.statusCode)
   {
+    case 202:
+      {
+        return true;
+      }
+    case 400:
+      {
+        return false;
+
+      }
+    default:
+      {
+        throw Exception('User login unexpected problem' + response.statusCode.toString()  );
+      }
+  }
+}
+
+Future<String> fetchToken() async {
+  Map<String, dynamic> map = {
     'username': 'marcogold',
     'password': 'marcodeaur'
   };
 
+  final response = await http.post(
+    Uri.http("10.0.2.2:8000", "api-token-auth/"),
+    body: jsonEncode(map),
+    headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+  );
 
-final response =
-    await http.post(Uri.http("localhost:8000", "api-token-auth"),
-      body: jsonEncode(map),
-    );
-}
+  if (response.statusCode == 200) {
+    var token = json.decode(response.body)['token'];
+    return token;
+  }
 
-
-Future<String> fetchToken() async
-{
-  final response = await http.get(Uri.http("localhost", "api-token-auth"));
-
-  if (response.statusCode ==200)
-    {
-      var token =json.decode(response.body)['token'];
-      return token;
-    }
-
-  return "";
+  throw Exception('Couldn\'t get the token');
 }
